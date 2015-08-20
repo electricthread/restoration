bbabb5ebe1a7aecc09caf06cbfbc41c780160eec
Evan Willhite <ewillhite@centresource.com>
2015-08-20 08:51:07 -0500

    first tpl

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,9 @@ $(function() {
     $("html, body").animate({scrollTop:$('.wrapper section:first-child').offset().top}, 1000);
   })
 
-
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
 
 });
